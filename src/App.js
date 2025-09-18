bff6baf6a70f714e9a252fafced4792f3c07e885
synmade <137505915+synmade@users.noreply.github.com>
2025-09-18 10:49:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DecryptedText from "./DecryptedText";
 import MagicBento from "./MagicBento";
 //import Dock from "./Dock";
