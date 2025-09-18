709840e1f426b9adf75883525ac7742c8eac68a7
synmade <137505915+synmade@users.noreply.github.com>
2025-09-18 06:36:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DecryptedText from "./DecryptedText";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>Welcome to SYNmade</h1>
+
+      <p style={{ margin: "2rem 0", fontSize: "1.5rem" }}>
+        Hover the text to decrypt:
+      </p>
+      <DecryptedText text="Hover me!" />
+
+      <p style={{ margin: "2rem 0", fontSize: "1.5rem" }}>
+        Custom speed and characters:
+      </p>
+      <DecryptedText
+        text="Customize me"
+        speed={100}
+        maxIterations={20}
+        characters="ABCD1234!?"
+        className="revealed"
+        parentClassName="all-letters"
+        encryptedClassName="encrypted"
+      />
+
+      <p style={{ margin: "2rem 0", fontSize: "1.5rem" }}>
+        Animate when in view:
+      </p>
+      <div style={{ marginTop: "4rem" }}>
+        <DecryptedText
+          text="This text animates when in view"
+          animateOn="view"
+          revealDirection="center"
+        />
+      </div>
     </div>
   );
 }
