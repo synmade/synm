1799fb8ff67f9b6403d2214084d3be9c286bb4b8
synmade <137505915+synmade@users.noreply.github.com>
2025-09-19 05:51:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -97,6 +97,9 @@ function Home() {
         glowColor="132, 0, 255"
       />
     </section>
+
+  <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/7gmszk7lYOwxQxRurFHCMd?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+  
   );
 }
 
